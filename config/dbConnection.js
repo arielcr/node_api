f562748b19b8eccc083c456f22b0077308115e60
Ariel Orozco <aorozco@fh.org>
2017-05-30 12:53:29 -0600

    Added local mongo connection string

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,8 @@
 module.exports = {
 
     getMongoConnection: function(){
-        return 'mongodb://arielcr:sampledb@ds061984.mlab.com:61984/nodesampleapi';
+        //return 'mongodb://arielcr:sampledb@ds061984.mlab.com:61984/nodesampleapi';
+        return 'mongodb://localhost:27017/presupuesto';
     }
 
 };
