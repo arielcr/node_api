63d086846ec482741c5dd2a9a1d721ca23e6f85d
Ariel Orozco <aorozco@fh.org>
2017-05-31 15:22:15 -0600

    Add cloud atlas connection string

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,9 @@
 module.exports = {
 
     getMongoConnection: function(){
+        return 'mongodb://testuser:testpass@aor-shard-00-00-8uov5.mongodb.net:27017,aor-shard-00-01-8uov5.mongodb.net:27017,aor-shard-00-02-8uov5.mongodb.net:27017/presupuesto?ssl=true&replicaSet=AOR-shard-0&authSource=admin';
         //return 'mongodb://arielcr:sampledb@ds061984.mlab.com:61984/nodesampleapi';
-        return 'mongodb://localhost:27017/presupuesto';
+        //return 'mongodb://localhost:27017/presupuesto';
     }
 
 };
