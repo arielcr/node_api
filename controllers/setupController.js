180c35f2fe950d9da99df2174d2473ecf5c8d720
Ariel Orozco <aorozco@fh.org>
2017-06-05 20:32:05 -0600

    ip whitelisting

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -4,12 +4,13 @@ module.exports = function (server, restify, restifyValidator) {
     server.use(restify.bodyParser());
     server.use(restifyValidator);
 
+    // BasicAuth
     server.use(restify.authorizationParser());
     server.use(function (req, res, next) {
         var apiKeys = {
             'user1': 'TNPGABh69Dz9U6PVLpxgtUhz7PBm2mwA'
         };
-        if (typeof(req.authorization.basic) === 'undefined' || !apiKeys[req.authorization.basic.username] || req.authorization.basic.password !== apiKeys[req.authorization.basic.username]){
+        if (typeof (req.authorization.basic) === 'undefined' || !apiKeys[req.authorization.basic.username] || req.authorization.basic.password !== apiKeys[req.authorization.basic.username]) {
             var response = {
                 'status': 'failure',
                 'data': 'You must specify a valid API key'
@@ -21,4 +22,12 @@ module.exports = function (server, restify, restifyValidator) {
         }
         return next();
     });
+
+    server.use(function (req, res, next) {
+        var whitelistedIps = ['111.222.333.444'];
+        var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+
+    });
+
 };
